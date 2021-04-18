d36724d85dc9e7b50c22ac719c4ffeac13618d29
jiawei <jiaweilim97@gmail.com>
2021-04-18 23:23:43 +0800

    Initial commit.

---
+++ datagetter/src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hhh lpr fff" class="layout">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .q-layout
+    background-color: $background !important
+    height: 400px
+    width: 400px
+    overflow: auto
+  .q-page-container
+    background-color: $background !important
+</style>
---
+++ datagetter/src/pages/PopupPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+      <q-btn label="hello" flat class="test"/>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PopupPage'
+}
+</script>
+
+<style lang="sass" scoped>
+.test
+    color: $magenta !important
+</style>
